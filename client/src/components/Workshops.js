f7031bd4d28336420c9a6d9226fdb0ee1e5a5a7b
nisoojadhav <nisoojadhav@gmail.com>
2022-09-12 17:53:51 +0530

    loading logic and spinner

--- client/src/components/Workshops.js
+++ client/src/components/Workshops.js
@@ -1,18 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import RandomImage from "./RandomImage";
+import { InfinitySpin } from "react-loader-spinner";
 
 function Posts() {
     const [posts, setPosts] = useState([]);
+    const [spinner, setSpinner] = useState(false);
+
     useEffect(() => {
+        setSpinner(true);
         axios
             .get("/posts")
             .then((res) => {
                 if (res) {
+                    setSpinner(false);
                     setPosts(res.data)
-                }
-                else {
-                     setPosts(res.data="<p style={{zIndex:6,color:'#fff'}}>loading...</p>")
                 }
             })
             .catch(
@@ -24,10 +26,22 @@ function Posts() {
         <>
             <RandomImage />
             <h1 className='main-head' style={{ paddingBottom: "1.4rem" }}>Workshops</h1>
-						
+
             <div style={{ width: "90%", textAlign: "center", margin: "0% 5%" }}>
 
-                {posts ?
+                {spinner ?
+                    (
+                        <>
+                            <center style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "60vh" }}>
+                                <InfinitySpin
+                                    width="200"
+                                    color="#42d392"
+                                    wrapperStyle
+                                    wrapperClass
+                                />
+                            </center>
+                        </>
+                    ) :
                     (
                         <div className="belowWorkshop">
                             {posts.map((post) => {
@@ -44,7 +58,7 @@ function Posts() {
                                         <p>{post.content}</p>
                                         <span>workshop tenure: </span>
                                         <p>{post.fromDate} ~ {post.toDate}</p>
-										<span>instructor: </span>
+                                        <span>instructor: </span>
                                         <p>{post.instructor}</p>
                                         <center>
                                             <hr color="#eee" />
@@ -52,11 +66,6 @@ function Posts() {
                                     </div>
                                 );
                             })}
-                        </div>
-                    ) :
-                    (
-                        <div className="belowWorkshop">
-                            return(<p style={{ color: "#fff" }}>loading...</p>);
                         </div>
                     )
                 }
